2d725b288ae71028221953e7abf830311d6b2caf
Beben-sd <lepron.e2202958@etud.univ-ubs.fr>
2025-12-01 14:37:08 +0000

    MAJ réponse

--- postgresql/reponses.sql
+++ postgresql/reponses.sql
@@ -1,6 +1,13 @@
--- SELECT * FROM clients;
--- SELECT nom, email FROM clients;
--- SELECT DISTINCT statut FROM commandes;
+-- SELECT * FROM clients WHERE date_inscription BETWEEN '2025-01-01' and '2025-12-31';
+-- SELECT nom, email FROM clients WHERE nom LIKE '%e%';
+-- SELECT * FROM clients WHERE email IS NULL;
+-- SELECT * FROM clients WHERE client_id BETWEEN 5 AND 10;
+-- SELECT * FROM clients WHERE nom NOT LIKE 'M%';
+-- SELECT * FROM clients WHERE date_inscription<'2023-01-01' OR nom LIKE '%ad%';
+-- SELECT * FROM clients WHERE email IN ('alice.dupont@example.com', 'karim.haddad@example.com');
+-- SELECT * FROM clients WHERE date_inscription BETWEEN '2024-01-01' AND '2024-03-31';
+-- SELECT * FROM clients WHERE nom NOT LIKE 'Alice Dupont';
+
 
 
 
