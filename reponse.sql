72a43bb8f3c483aeae4577e5d304034c9c4192a6
Alexis-g14 <guyalexis828@gmail.com>
2025-12-01 14:41:11 +0000

    New MAJ quesions

--- reponse.sql
+++ reponse.sql
@@ -1,5 +1,28 @@
-SELECT * FROM produits;
-SELECT nom FROM clients WHERE email LIKE '%example.com';
-SELECT produit_id, SUM(quantite) FROM lignes_commandes GROUP BY produit_id;
+-- Questions pratiques
 
-SELECT DISTINCT statut FROM commandes;
+-- 1/
+SELECT * FROM  clients WHERE date_inscription BETWEEN '2025-01-01' AND '2025-12-31';
+
+-- 2/
+SELECT nom, email FROM clients WHERE nom LIKE '%e%';
+
+-- 3/
+SELECT * FROM clients WHERE email IS NULL;
+
+-- 4/
+SELECT * FROM  clients WHERE client_id BETWEEN 5 AND 10;
+
+-- 5/
+SELECT * FROM clients WHERE nom NOT LIKE 'M%';
+
+-- 6/
+SELECT * FROM clients WHERE date_inscription < '2023-01-01';
+
+-- 7/
+SELECT * FROM clients WHERE email in ('alice.dupont@example.com', 'claire.leroy@yahoo.com');
+
+-- 8/
+SELECT * FROM clients WHERE date_inscription BETWEEN '2024-01-04' AND '2024-03-01';
+
+-- 9/
+SELECT * FROM clients WHERE nom NOT LIKE '%Dupont%';
