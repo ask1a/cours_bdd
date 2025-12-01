b72f7d42f933376c367c24f33055012fb8c5c9ef
mledurand <le-durand.e2203154@etud.univ-ubs.fr>
2025-12-01 14:36:32 +0000

    reponse sql

--- postgresql/response.sql
+++ postgresql/response.sql
@@ -1,3 +1,9 @@
-select * from clients where email != '';
-SELECT produit_id, SUM(quantite) FROM lignes_commandes GROUP BY produit_id;
-SELECT * from produits;
+SELECT * FROM clients WHERE date_inscription BETWEEN '2025-01-01' AND '2025-12-31';
+SELECT nom,email FROM clients WHERE nom LIKE'%e%' and email LIKE '%e%';
+SELECT nom FROM clients WHERE email IS NULL;
+SELECT nom FROM clients WHERE client_id BETWEEN 5 and 10;
+SELECT * FROM clients WHERE nom NOT LIKE 'M%';
+SELECT * FROM clients WHERE date_inscription BETWEEN '2023-01-01' AND '2023-12-31' AND nom LIKE '%ad%';
+SELECT * FROM clients WHERE email IN ('alice@mail.com', 'bob@mail.com');
+SELECT * FROM clients WHERE date_inscription BETWEEN '2024-01-01' AND '2024-03-31';
+SELECT * FROM clients WHERE nom != 'Dupont';
